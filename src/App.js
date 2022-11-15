de6d4976fa6a29db39ee0505b7c2f57364dde3f7
Miguel Coria <miky@Miguels-MacBook-Air.local>
2022-11-14 22:15:23 -0600

    added bobis

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is my app
+            Bobis te amo!!!
           </p>
           <LambdaDemo />
         </header>
