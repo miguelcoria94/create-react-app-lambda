6558ae733697cfb06901e9b424e2f7a30facedf3
Miguel Coria <miky@Miguels-MacBook-Air.local>
2022-11-14 22:27:25 -0600

    added a counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react"
+import React, {useState}from "react"
 import "./App.css"
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    setCount(count + 1)
+  }
+
+  function handleSub(){
+    setCount(count - 1)
+  }
+
+
   return (
-    <h1>I love you Emilis!!</h1>
+    <>
+      <h1>
+        {count}
+      </h1>
+      <button onClick={handleClick}>Add One</button>
+      <button onClick={handleSub}>Subtract One</button>
+    </>
   )
 }
 
